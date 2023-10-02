7ff2de96d46c81616196810e06ddcc2f564fb070
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2023-10-02 13:29:21 +0000

    tut 2

---
+++ script.js
@@ -0,0 +1,37 @@
+// tutorial #2
+
+/* let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+points = 50;
+console.long(points);
+
+var score = 75;
+console.log(score);
+
+// strings
+console.log('hello, world');
+
+let email = 'user@gmail.com';
+console.log(email); */
+
+// string concatenation
+let firstName = 'angelina';
+let lastName = 'phan';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
