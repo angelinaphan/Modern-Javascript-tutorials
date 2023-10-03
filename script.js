6d1805e571f169eb3585ff176b00d5f13a6b5e3b
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2023-10-03 14:39:02 +0000

    tutorial #2

--- script.js
+++ script.js
@@ -13,17 +13,18 @@ points = 50;
 console.long(points);
 
 var score = 75;
-console.log(score);
+console.log(score); */
 
+/*
 // strings
 console.log('hello, world');
 
 let email = 'user@gmail.com';
-console.log(email); */
+console.log(email);
 
 // string concatenation
-let firstName = 'angelina';
-let lastName = 'phan';
+let firstName = 'Angelina';
+let lastName = 'Phan';
 
 let fullName = firstName + ' ' + lastName;
 console.log(fullName);
@@ -35,3 +36,76 @@ console.log(fullName[0]);
 console.log(fullName.length);
 
 // string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index); */
+
+/*
+// common string methods
+let email = 'angelina@gmail.com';
+
+// let result = email.lastIndexOf('n');
+
+// let result = email.slice(0,5);
+
+// let result = email.substr(2,10);
+
+// let result = email.replace('e', 'i');
+
+let result = email.replace('g', 'i');
+
+console.log(result); */
+
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius**2;
+
+// order of operation - B I D M A S
+
+// let result = 5 * (10 - 3)**2;
+
+// console.log(result);
+
+let likes = 10;
+
+// likes = likes + 1;
+// likes++;
+// likes--;
+
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
+
+// console.log(likes);
+
+// NaN - not a number
+
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+// let result = 'the blog has ' + likes + ' likes';
+// console.log(result);
+
+// template strings
+const title = 'best reads of 2019';
+const author = 'mario';
+const likes = 30;
+
+// concatenation way 
+
+
+// template string way
+
+
+// creating html templates
