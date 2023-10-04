91ef685ed0428be3bee0693ca962cdb7efd82800
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2023-10-04 15:51:12 +0000

    tutorial #2

--- script.js
+++ script.js
@@ -98,15 +98,76 @@ console.log(result); */
 // console.log(result);
 
 
-// template strings
+/* // template strings
 const title = 'best reads of 2019';
 const author = 'mario';
 const likes = 30;
 
 // concatenation way 
-
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
 
 // template string way
-
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
+console.log(result);
 
 // creating html templates
+let html = `
+<h2>${title}</h2>
+<p>By ${author}</p>
+<span>This blog has ${likes} likes</span>
+`;
+
+console.log(html); */
+
+
+// let ninjas = ['shaun', 'rya', 'chun-li'];
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20, 25, 30, 35];
+// console.log(ages[2]);
+
+// let random = ['shaun', 'crystal', 30, 20];
+// console.log(random);
+
+// console.log(ninjas.length);
+
+// array methods
+
+// let result = ninjas.join(',');
+// let result = ninjas.indexOf('chun-li');
+// let result = ninjas.concat(['ken', 'crystal']);
+// let result = ninjas.push('ken');
+// result = ninjas.pop();
+
+// console.log(result);
+
+// let age = null;
+
+// console.log(age, age + 3, `the age is ${age}`);
+
+
+// booleans & comparisons
+console.log(true, false, "true", "false");
+
+// methods can return booleans
+let email = 'angelina@gmail.com';
+let names = ['mario', 'luigi', 'toad'];
+
+// let result = email.includes('!');
+// let result = names.includes('bowser');
+
+// console.log(result);
+
+// comparison operators
+let age = 25;
+
+console.log(age == 25);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age > 20);
+console.log(age < 20);
+console.log(age <= 25);
+console.log(age >= 25);
