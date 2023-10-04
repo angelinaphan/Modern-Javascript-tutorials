d2f89038f25fb899afb7c3f8423f09b92f7acde2
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2023-10-04 17:11:42 +0000

    done with tutorial #2

--- script.js
+++ script.js
@@ -150,11 +150,11 @@ console.log(html); */
 
 
 // booleans & comparisons
-console.log(true, false, "true", "false");
+// console.log(true, false, "true", "false");
 
 // methods can return booleans
-let email = 'angelina@gmail.com';
-let names = ['mario', 'luigi', 'toad'];
+// l et email = 'angelina@gmail.com';
+// let names = ['mario', 'luigi', 'toad'];
 
 // let result = email.includes('!');
 // let result = names.includes('bowser');
@@ -162,12 +162,53 @@ let names = ['mario', 'luigi', 'toad'];
 // console.log(result);
 
 // comparison operators
-let age = 25;
+// let age = 25;
 
-console.log(age == 25);
+/* console.log(age == 25);
 console.log(age == 30);
 console.log(age != 30);
 console.log(age > 20);
 console.log(age < 20);
 console.log(age <= 25);
-console.log(age >= 25);
+console.log(age >= 25); */
+
+/* let name = 'shaun';
+
+console.log(name == 'shaun');
+console.log(name == 'shaun');
+console.log(name > 'crystal');
+console.log(name > 'shaun');
+console.log(name > 'crystal'); */
+
+// let age = 25;
+
+// loose comparison (different types can still be equal)
+
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
+
+// strict comparison (different types cannot be equal)
+/* console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25'); */
+
+
+//type conversation 
+// let score = '100';
+
+// score = Number(score);
+// console.log(score +1);
+// console.log(typeof score);
+
+// let result = Number('hello');
+// let result = String(50);
+// let result = Boolean(0);
+
+// let result = Boolean('');
+
+// console.log(result, typeof result);
+
+// tutorial #3
