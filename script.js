b8633b5f4eaf6f8dcb6fb1ba02f657f75ffd9108
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2023-10-10 11:21:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -211,4 +211,155 @@ console.log(age !== '25'); */
 
 // console.log(result, typeof result);
 
+
 // tutorial #3
+
+// for loops
+
+/* for(let i = 0; i < 5; i++){
+  console.log('in loop:', i);
+}
+
+console.log('loop finished'); */
+
+/* const  names = ['shaun', 'mario', 'luigi'];
+
+for(let i = 0; i < names.length; i++){
+  // console.log(names[i]);
+  let html = '<div>${names[i]}</div>';
+  console.log(html);
+} */
+
+// while loops
+// const names = ['shaun', 'mario', 'luigi'];
+// let i = 0;
+
+// while(i < 5){
+// console.log('in loop: ', i);
+//  i++;
+// }
+
+/* let i = 0;
+while(i < names.length){
+  console.log(names[i]);
+  i++;
+} */
+
+// do while loops
+
+/* let i = 5;
+
+do {
+  console.log('val of i is: ', i);
+  i++;
+} while(i < 5); */
+
+
+// if statements
+/* const age = 25;
+
+if(age > 20){
+  console.log('you are over 20 years old');
+}
+
+const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+if(ninjas.length > 3){
+  console.log("that's a lot of ninjas");
+} */
+
+// else if statements      // logical operatos - OR || and AND &&
+/* const password  = 'p@ss12'; 
+
+if(password.length >= 12 && password.includes('@')){
+  console.log('that password is mighty strong');
+} else if(password.length >= 8 || password.includes('@') && password.length > 5){
+  console.log('that password is strong enough!');
+} else {
+  console.log('password is not strong enough!');
+} */
+
+
+// logical NOT (!)
+
+/* let user = false;
+
+if(!user){
+  console.log('you must be logged in to continue');
+}
+
+console.log(!true); 
+console.log(!false); */
+
+
+// break and continue
+
+/* const scores = [50, 25, 0, 30, 100, 20, 10];
+
+for(let i = 0; i < scores.length; i++){
+
+  if(scores[i] === 0){
+    continue;
+  }
+
+  console.log('your score: ', scores[i]);
+
+  if(scores[i] === 100){
+    console.log('congrats, you got the top score!');
+    break;
+  }
+  
+} */
+
+
+// switch statements
+// const grade = 'D';
+
+
+// using if statements
+/* if(grade === 'A'){
+
+  { else if(grade === 'B')}
+
+  { else if(grade === 'C')}
+  
+  { else if(grade === 'D')}
+  
+  { else if(grade === 'E')}
+} */ 
+
+
+// tutorial #4
+  // function expression
+ /* const speak = function(){
+    console.log('good day!');
+  }; */
+
+
+// greet();
+// greet();
+// greet();
+
+/* speak();
+speak();
+speak();
+
+// function declaration
+function greet(){
+    console.log('hello there');
+} */
+
+// arguments & parameters
+
+/* const speak = function(name = 'angie', time = 'night'){
+  console.log(`good ${time} ${name}`);
+};
+
+speak();
+speak('shaun'); */
+
+// returing values
+
+const speak = function(name = 'angie', time = 'night'){
+  console.log(`good ${time} ${name}`);
+};
