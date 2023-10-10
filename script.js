3ede0969b8ef4bd126f812c828965eb38f91147f
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2023-10-10 11:45:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -360,6 +360,34 @@ speak('shaun'); */
 
 // returing values
 
-const speak = function(name = 'angie', time = 'night'){
+/* const speak = function(name = 'angie', time = 'night'){
   console.log(`good ${time} ${name}`);
-};
+}; */
+
+/* const calcArea = function(radius){
+  let area = 3.14 * radius**2;
+} */
+
+// arrow function
+/* const calcArea = radius => 3.14 * radius**2;
+
+const area = calcArea(5);
+console.log('area is:', area); */
+
+// practise arrow functions
+
+/* const greet = function(){
+  return 'hello, world';
+} */
+
+// const greet = () => 'hello, world';
+// const result = greet();
+// console.log(result);
+
+const bill = function(products, tax){
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i] + products[i] * tax;
+  }
+  return total;
+}
